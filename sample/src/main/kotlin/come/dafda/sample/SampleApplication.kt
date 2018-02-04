c714e9c7c5a272dac7f2236c582916800dacb00e
jdafda <jitudafda@gmail.com>
2018-02-03 20:43:29 -0500

    added spring boot kotlin app

---
+++ sample/src/main/kotlin/come/dafda/sample/SampleApplication.kt
@@ -0,0 +1,11 @@
+package come.dafda.sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SampleApplication>(*args)
+}
